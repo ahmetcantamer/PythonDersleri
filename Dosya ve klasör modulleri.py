8a9fadfff1f10c0ad7ada68ab6043019c246cdd8
Ahmetcan Tamer <xyz470808@gmail.com>
2025-04-16 15:13:57 +0300

    The subject about folders and files were covered

---
+++ Dosya ve klasör modulleri.py
@@ -0,0 +1,14 @@
+import os
+'''
+print(os.getcwd())  #şu anki çalıştığımız dizini gösterir
+print(os.listdir()) #açılan klasördeki dosyaları gösterir
+print(os.listdir('D:\\Users\\ahmet\\Masaüstü')) #Hangi klasörü görmek istiyorsak onu yazarız
+print(os.chdir('D:\\Users\\ahmet\\Masaüstü\\Python örnekleri')) #Klasör değiştirir
+'''
+
+# dosyalar = os.listdir()
+# for eleman in dosyalar:
+#     print(eleman)
+
+#os.mkdir('D:\\Users\\ahmet\\Masaüstü\\Python örnekleri\\Deneme') #Yeni klasör oluşturur
+#os.rmdir('D:\\Users\\ahmet\\Masaüstü\\Python örnekleri\\Deneme') #Klasörü siler
